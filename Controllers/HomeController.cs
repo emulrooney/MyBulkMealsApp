dd1110a314110d6c4b91eaa5e4652aa532d7f9b3
Evan Mulrooney <evanmulrooney@gmail.com>
2020-11-08 14:24:38 -0500

    O.9 - Add proper About/Contact page w dummy content

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -40,6 +40,16 @@ namespace MyBulkMealsApp.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
