b2d455cf939fac7fb54818c26568101d97809997
Evan Mulrooney <evanmulrooney@gmail.com>
2020-12-03 13:54:09 -0500

    Misc - Added SimpleConfig class for testing

---
+++ Data/SimpleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBulkMealsApp.Data
+{
+    /// <summary>
+    /// Minimal class to have static configuration info in one place. 
+    /// </summary>
+    public static class SimpleConfig
+    {
+        public static int VerificationNotificationTrigger { get; } = 20;
+        public static int ItemsPerPage { get; } = 25;
+    }
+}
